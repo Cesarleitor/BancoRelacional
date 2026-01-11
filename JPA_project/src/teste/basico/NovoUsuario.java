f30ce6a122681c2259fb4fab882b2f8c63a3dc17
Cesar Augusto <100009078+Cesarleitor@users.noreply.github.com>
2026-01-10 21:29:18 -0300

    configuração jpa com sql

---
+++ JPA_project/src/teste/basico/NovoUsuario.java
@@ -0,0 +1,24 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import modelo.basico.Usuario;
+
+public class NovoUsuario {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence
+                .createEntityManagerFactory("jpa");
+        EntityManager em = emf.createEntityManager();
+
+        Usuario novoUsuario = new Usuario("Adriana", "adriana@gmail.com");
+
+        em.getTransaction().begin();
+        em.persist(novoUsuario);
+        em.getTransaction().commit();
+
+        em.clear();
+        emf.close();
+    }
+}
